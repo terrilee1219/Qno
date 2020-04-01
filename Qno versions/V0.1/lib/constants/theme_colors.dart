27010e6008d9df138957e0d88f43e34fdf92436c
Nathan Garces <nathangarces71@hotmail.com>
2020-04-01 17:04:11 +0200

    Removed the old flutter files.

--- Qno versions/V0.1/lib/constants/theme_colors.dart
+++
@@ -1,9 +0,0 @@
-/// The main app theme colors.
-
-//Imports
-import 'dart:ui';
-
- class ThemeColors{
-  static const Color primaryColor = Color(0xFF000000);
-  static const Color secondaryColor = Color(0xFFB5B5B5);
- }
---
+++ Qno versions/qno_app_client/lib/main.dart
@@ -0,0 +1,31 @@
+/// Qno is a smart coaster paging app designed with flutter.
+/// This is Qno v0.1 and is a basic proof of concept which has no sophisticated communication between client and nfc.
+
+
+// Imports //
+//Packages
+import 'package:flutter/material.dart';
+
+//Screens
+import './screens/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Qno",
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      //home: HomeScreen(), Set the current screen being developed.
+      routes: {
+        HomeScreen.routeName : (ctx) => HomeScreen(),
+      },
+
+    );
+  }
+}
