253afb2e37801c325067993acfc42b4fc3a97378
James Watters <53174355+JamesWattersIV@users.noreply.github.com>
2020-04-22 15:49:57 +0200

    Add feedback after tap functionality (#21)

--- Qno versions/qno_app_client/lib/providers/nfc.dart
+++ Qno versions/qno_app_client/lib/providers/nfc.dart
@@ -1,66 +1,24 @@
-import 'package:nfc_in_flutter/nfc_in_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-import 'dart:async';
 class NFCReader{
 
-  bool _isNfcSupported = false;
-  Stream<NDEFMessage> _stream;
-  String _nfcMessage;
-
-  //Read a NFC tag when app is opened
-  void readNFC(){
-  //Opens a NFC Listening Stream for the device
-    _stream = NFC.readNDEF();
-
-  //Manage a tap on a tag
-    _stream.listen((NDEFMessage message) {
-  String result = '${message.payload}'.substring(2); //Substring to remove 'en' from tag payload message. i.e enTagTapped -> TagTapped
-  setNfcMessage(result);
-    print(_nfcMessage);
-  connectToServer();
-  });
-  }
-
-  //Check to see if device supports NFC
-  Future<void> setNfcSupport() async {
-    await NFC.isNDEFSupported
-        .then((bool isSupported) {
-      _isNfcSupported = isSupported;
-    });
-  }
-
-  //Returns true if device supports NFC
-  Future<bool> getNfcSupport() async{
-    await setNfcSupport();
-    return _isNfcSupported;
-  }
-
   //Connect to server when tag tapped
-  //must increment the order number
-  //integrate seemlessly -> when does the order number generate and by who? i..e machine side
   Future<void> connectToServer() async{
     const url = "https://qno-technologies.firebaseio.com/hello_msg.json";
+    print("Tag Contect: $tagContent");
 
     try {
       final response = await http.get(url);
       final extractedData = json.decode(response.body);
       if (extractedData == null) {
+        print('No Message From Server: $response');
         return;
       }
-
-      //Print Message from server
-      print('Extracted Data: $extractedData');
+      print('Message From Server: $response');
     } catch (error) {
       print('Error: $error');
     }
   }
-
-  //Set the nfcMessage from tapped tag
-  void setNfcMessage(String val){
-    _nfcMessage = val;
-  }
-
 }
 
