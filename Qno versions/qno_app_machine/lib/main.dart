bf9a374914f39a02ead40ab5550164033cb23873
rubin <safedupkid@googlemail.com>
2020-04-29 09:40:27 +0200

    Latest backend update

--- Qno versions/qno_app_machine/lib/main.dart
+++ Qno versions/qno_app_machine/lib/main.dart
@@ -1,37 +1,109 @@
-/// Qno is a smart coaster paging app designed with flutter.
-/// This is Qno v0.1 and is a basic proof of concept which has no sophisticated communication between client and nfc.
+import 'dart:convert';
+import 'dart:io';
 
-//Package Imports
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-
-//Constants
-import './constants/theme_colors.dart';
-
-//Screens
-import './screens/home_screen.dart';
-
-//Providers
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Dismissible',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: MyHomePage(title: 'QNO HealthCare Kiosk'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //real data here
+  final itemsList = List<String>.generate(10, (n) => "073019561${n}");
+
+  ListView generateItemsList() {
+    return ListView.builder(
+      itemCount: itemsList.length,
+      itemBuilder: (context, index) {
+        return Dismissible(
+          key: Key(itemsList[index]),
+          child: InkWell(
+              onTap: () {
+                print("${itemsList[index]} clicked");
+              },
+              child: ListTile(title: Text('${itemsList[index]}'))),
+          background: slideLeftBackground(),
+          secondaryBackground: slideLeftBackground(),
+          onDismissed: (direction){
+            writeUserData(itemsList[index]);
+            itemsList.removeAt(index);
+            },
+        );
+    },
+    );
+  }
+
+   void  writeUserData (String phoneNo) async {
+     var now = new DateTime.now();
+     dynamic dateTime = new DateFormat("dd-MM-yyyy hh:mm:ss").format(now);
+
+     final db = Firestore.instance;
+     db.collection("requests").document().setData({"phoneNo": phoneNo, "subDate" : dateTime});
+}
+
+
+  Widget slideLeftBackground() {
+    return Container(
+      color: Colors.red,
+      child: Align(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+            Text(
+              " Delete",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+              ),
+              textAlign: TextAlign.right,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+          ],
+        ),
+        alignment: Alignment.centerRight,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-        title: "Qno",
-        theme: ThemeData(
-          primarySwatch: ThemeColors.purpleSwatch,
-        ),
-        //home: HomeScreen(),
-        routes: {
-          HomeScreen.routeName : (ctx) => HomeScreen(),
-        },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: generateItemsList(),
     );
   }
 }
